2c5eed20945245f359c4f9423709ac353afbf6e1
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-11-19 15:26:49 +0100

    Fixed User import

--- backend/usos_backend/usos_api/serializers.py
+++ backend/usos_backend/usos_api/serializers.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import Group
 from rest_framework import serializers
-
+from .models import User
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
