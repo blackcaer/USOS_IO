8cdbe33a490c038ce1bf0c1a9ea10592417e0fdd
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-01 03:08:06 +0100

    Added models to admin site

--- backend/usos_backend/usos_api/admin.py
+++ backend/usos_backend/usos_api/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import User
+from .models import (
+    User, Parent, Student, Teacher, StudentGroup,
+    SchoolSubject, Grade, GradeColumn, Attendance,
+    Meeting, ConsentTemplate, ParentConsent, ScheduledMeeting,
+    CategoryUserStatus, CategoryStudentGroup, CategoryGradeValue, CategoryAttendanceStatus
+)
+from django.contrib.auth.models import Group
 
 admin.site.register(User)
+admin.site.register(Parent)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(StudentGroup)
+admin.site.register(SchoolSubject)
+admin.site.register(Grade)
+admin.site.register(GradeColumn)
+admin.site.register(Attendance)
+admin.site.register(Meeting)
+admin.site.register(ConsentTemplate)
+admin.site.register(ParentConsent)
+admin.site.register(ScheduledMeeting)
+admin.site.register(CategoryUserStatus)
+admin.site.register(CategoryStudentGroup)
+admin.site.register(CategoryGradeValue)
+admin.site.register(CategoryAttendanceStatus)
