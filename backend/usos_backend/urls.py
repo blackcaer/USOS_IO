affdaf547a9bab5c3fa38c4b4b13badc25580f48
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-02 22:06:42 +0100

    Refactor URL routing to use UserCreateViewSet and MeetingViewSet; cleaning code

--- backend/usos_backend/urls.py
+++ backend/usos_backend/urls.py
@@ -1,69 +1,25 @@
-"""
-URL configuration for usos_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-
+from rest_framework.routers import DefaultRouter
 from .usos_api.views import (
     StudentViewSet, TeacherViewSet, ParentViewSet, GradeViewSet,
-    CurrentUserView, UserInfoView,  GradeColumnView,
+    CurrentUserView, UserInfoView, GradeColumnView,
     GradeListCreateView, GradeDetailView, ScheduleView, 
-    ConsentTemplateView, FeedView,UserViewSet
+    ConsentTemplateView, FeedView, UserCreateViewSet, MeetingViewSet
 )
-
-
-"""from .usos_api.views import (
-    UserViewSet, GroupViewSet, UserView, StudentGroupViewSet, GradeViewSet, GradeColumnViewSet, 
-    ScheduledMeetingViewSet, ParentConsentViewSet, ConsentTemplateViewSet
-)
-"""
-router = routers.DefaultRouter()
-"""router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)"""
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-"""urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]"""
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+import datetime
 
 
 
-"""router.register(r'student-groups', StudentGroupViewSet, basename='studentgroup')
-router.register(r'grades', GradeViewSet, basename='grade')
-router.register(r'grade-columns', GradeColumnViewSet, basename='gradecolumn')
-router.register(r'meetings', ScheduledMeetingViewSet, basename='meeting')
-router.register(r'econsent', ParentConsentViewSet, basename='parentconsent')
-router.register(r'econsent-templates', ConsentTemplateViewSet, basename='consenttemplate')
-
-urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
-    path('user/<int:pk>/', UserViewSet.as_view(), name='user-detail'),
-    path('', include(router.urls)),
-]"""
-
-
-
+router = DefaultRouter()
 router.register('students', StudentViewSet, basename='student')
 router.register('teachers', TeacherViewSet, basename='teacher')
 router.register('parents', ParentViewSet, basename='parent')
 router.register('grades', GradeViewSet, basename='grade')
-router.register('users', UserViewSet, basename='user')
+router.register('users', UserCreateViewSet, basename='user')
+router.register('meetings', MeetingViewSet, basename='meeting')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -78,4 +34,3 @@ urlpatterns = [
     path('econsent/templates/<int:template_consent_id>/', ConsentTemplateView.as_view(), name='consent_template_detail'),
     path('feed/<int:user_id>/', FeedView.as_view(), name='user_feed'),
 ]
-
