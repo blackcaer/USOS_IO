8c76ddc74b642e0c286bc0033531009dca63f9fc
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-05 18:50:15 +0100

    Deleted pagination to supress warnings xd

--- backend/usos_backend/settings.py
+++ backend/usos_backend/settings.py
@@ -128,9 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Django Rest Framework
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 10
+# }
 
 
